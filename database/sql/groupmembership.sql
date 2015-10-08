137b6045977a434e8793cd928002de72a22749d9
marscher <m.scherer@fu-berlin.de>
2015-10-08 18:04:44 +0200

    changes

--- database/sql/groupmembership.sql
+++ database/sql/groupmembership.sql
@@ -13,5 +13,5 @@ CREATE TABLE GroupMembership (
 ALTER TABLE GroupMembership ADD CONSTRAINT pk_groupmembership PRIMARY KEY (group_id,user_id);
 ALTER TABLE GroupMembership ADD CONSTRAINT  FOREIGN KEY () REFERENCES User ();
 ALTER TABLE GroupMembership ADD CONSTRAINT  FOREIGN KEY () REFERENCES User ();
-ALTER TABLE GroupMembership ADD CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES Group (id);
+ALTER TABLE GroupMembership ADD CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES Groups (id);
 ALTER TABLE GroupMembership ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES User (id);
