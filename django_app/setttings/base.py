32c01689bea16f7cdbaceacc1acc0fcb528cc065
marscher <m.scherer@fu-berlin.de>
2015-10-08 16:47:40 +0200

    import django app

---
+++ django_app/setttings/base.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ImproperlyConfigured
+ 
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+ 
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+
+
