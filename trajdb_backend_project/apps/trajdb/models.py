fad5101991035a96e80ce714360b9e079f327d58
marscher <m.scherer@fu-berlin.de>
2015-10-09 16:52:36 +0200

    added signal handlers for creation of user and traj

--- trajdb_backend_project/apps/trajdb/models.py
+++ trajdb_backend_project/apps/trajdb/models.py
@@ -9,76 +9,99 @@
 # into your database.
 from __future__ import unicode_literals
 
-from django.db import models
 from django.conf import settings
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
 from . import managers
 
+
 class Profile(models.Model):
+    """A Profile encapsulates an authenticated user. One authed user has
+       exactly one profile.
+    """
     # Relations
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         related_name="profile",
         verbose_name=_("user")
-        )
-    # Attributes - Mandatory
-    interaction = models.PositiveIntegerField(
-        default=0,
-        verbose_name=_("interaction")
-        )
+    )
     # Attributes - Optional
     # Object Manager
     objects = managers.ProfileManager()
- 
+
     # Custom Properties
     @property
     def username(self):
         return self.user.username
- 
+
     # Methods
- 
+
     # Meta and String
     class Meta:
         verbose_name = _("Profile")
         verbose_name_plural = _("Profiles")
         ordering = ("user",)
- 
+
     def __str__(self):
-        return self.user.username 
+        return self.user.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile_for_new_user(sender, created, instance, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
+
 
 class Setup(models.Model):
+    """ Setup contains the simulation setup like used topology and forcefield
+    parameters.
+    """
     id = models.IntegerField(primary_key=True)
     description = models.CharField(max_length=1000)
     pdb = models.CharField(max_length=4)
     program = models.CharField(max_length=20)
     program_version = models.CharField(max_length=10)
     topology = models.BinaryField()
-    topology_type = models.CharField(max_length=4) # TODO: replace with choices?
+    # TODO: replace with choices?
+    topology_type = models.CharField(max_length=4)
     forcefield_name = models.CharField(max_length=20)
     forcefield_parameters = models.BinaryField()
     forcefield_parameters_type = models.CharField(max_length=4)
 
 
 class Collection(models.Model):
+    """ A collection is associated to one setup and owner.
+    """
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    length = models.IntegerField()
-    n_atoms = models.IntegerField()
+    n_atoms = models.IntegerField(help_text='Number of atoms/particles in the simulation.')
+    cumulative_length = models.PositiveIntegerField(default=0)
     setup = models.ForeignKey(Setup)
     owner = models.ForeignKey(Profile)
 
-class Collectionownership(models.Model):
-    id = models.IntegerField(primary_key=True)
-    collection_id = models.ForeignKey(Collection)
-    profile_id = models.ForeignKey(Profile)
 
 class Trajectory(models.Model):
+    """Stores a trajectory file associated to a collection.
+    Has a unique hash (sha512).
+    Might refer to a parent trajectory from which the actual has been forked from.
+    """
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
+    length = models.PositiveIntegerField()
     parent_traj = models.ForeignKey('self', blank=True, null=True)
     collection = models.ForeignKey(Collection)
     uri = models.CharField(max_length=1000)
+    hash_sha512 = models.CharField(max_length=128, unique=True)
 
 
+@receiver(post_save, sender=Trajectory)
+def update_cumulative_simulation_len(sender, created, instance, **kw):
+    if created:
+        assert isinstance(instance, Trajectory)
+        instance.collection.cumulative_length += instance.length
+        instance.collection.save()
